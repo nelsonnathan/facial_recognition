5407f750d9a103918194a9a3e52bf10f26133b03
Nate Nelson <nathan.nelson99@gmail.com>
2021-11-17 14:05:21 -0700

    adds simple navigation bar

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_nav import Nav
+from flask_nav.elements import Navbar, View, Separator
 
+
+# initialization section
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+nav = Nav(app)
 
+
+# navigation section
+def my_navbar():
+    navbar = Navbar(title='recognition')
+    navbar.items = [View('Home', 'home')]
+
+    return navbar
+
+nav.register_element('my_navbar', my_navbar)
+
+
+
+# routes section
 @app.route('/')
 def home():
     return render_template("home.html")
